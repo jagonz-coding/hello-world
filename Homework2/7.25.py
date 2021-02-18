ed876661ecd8fc79d31df0913e0df3894348c8f0
jagonz-coding <77754243+jagonz-coding@users.noreply.github.com>
2021-02-18 11:32:25 -0600

    Update 7.25.py

--- Homework2/7.25.py
+++ Homework2/7.25.py
@@ -1,57 +1,57 @@
 #Name: Juan Gonzalez
 #ID: 1808943
 
+
+
 def exact_change(user_total):
-    dol = 0
-    qua = 0
-    dime = 0
-    nick = 0
-    pen = 0
-    while user_total > 0:
-        if user_total > 100:
-            dol += 1
-            user_total -= 100
-        elif user_total > 25:
-            qua += 1
-            user_total -= 25
-        elif user_total > 10:
-            dime += 1
-            user_total -= 10
-        elif user_total > 5:
-            nick += 1
-            user_total -= 5
-        elif user_total > 1:
-            pen += 1
-            user_total -= 1
-    if dol > 0:
-        if dol > 1:
-            print(dol, ' dollars')
-        elif dol == 1:
+    numpennies = int(user_total)
+    numdollars = 0
+    numquarters = 0
+    numdimes = 0
+    numnickels = 0
+    if numpennies == 0:
+        print('no change')
+    while numpennies >= 5:
+        if numpennies >= 100:
+            numdollars += 1
+            numpennies -= 100
+        elif numpennies >= 25:
+            numquarters += 1
+            numpennies -= 25
+        elif numpennies >= 10:
+            numdimes += 1
+            numpennies -= 10
+        elif numpennies >= 5:
+            numnickels += 1
+            numpennies -= 5
+    if numdollars > 0:
+        if numdollars > 1:
+            print(numdollars, 'dollars')
+        elif numdollars == 1:
             print('1 dollar')
-            
-    if qua > 0:
-        if qua > 1:
-            print(qua, ' quarters')
-        elif qua == 1:
+
+    if numquarters > 0:
+        if numquarters > 1:
+            print(numquarters, 'quarters')
+        elif numquarters == 1:
             print('1 quarter')
 
-    if dime > 0:
-        if dime > 1:
-            print(dime, ' dimes')
-        elif dime == 1:
+    if numdimes > 0:
+        if numdimes > 1:
+            print(numdimes, 'dimes')
+        elif numdimes == 1:
             print('1 dime')
 
-    if nick > 0:
-        if nick > 1:
-            print(nick, ' nickels')
-        elif nick == 1:
+    if numnickels > 0:
+        if numnickels > 1:
+            print(numnickels, 'nickels')
+        elif numnickels == 1:
             print('1 nickel')
 
-    if pen > 0:
-        if pen > 1:
-            print(pen, ' pennies')
-        elif pen == 1:
+    if numpennies > 0:
+        if numpennies > 1:
+            print(numpennies, 'pennies')
+        elif numpennies == 1:
             print('1 penny')
 
-
-exact_change(int(input()))
+exact_change(input())
