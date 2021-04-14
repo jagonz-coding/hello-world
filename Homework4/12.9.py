b23e46b14dba90585c9b0330f6b7b449cb697027
jagonz-coding <77754243+jagonz-coding@users.noreply.github.com>
2021-04-14 15:59:38 -0500

    Add files via upload

---
+++ Homework4/12.9.py
@@ -0,0 +1,14 @@
+#Name: Juan Gonzalez
+#ID: 1808943
+
+parts = input().split()
+name = parts[0]
+
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except Exception as ex:
+        age = 0
+    print('{} {}'.format(name, age))
+    parts = input().split()
+    name = parts[0]
